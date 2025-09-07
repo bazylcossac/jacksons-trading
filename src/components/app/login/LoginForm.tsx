09b2991d6b8504ab5df10a191a6e31e7b137d0ce
bazylcossac <bazyl.cossac@gmail.com>
2025-09-07 10:43:48 +0200

    add validation to form, add use chace and react compiler

--- src/components/app/login/LoginForm.tsx
+++ src/components/app/login/LoginForm.tsx
@@ -2,10 +2,24 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { FcGoogle as GoogleIcon } from 'react-icons/fc';
+import { Login, LoginSchema } from '@/@types/app/login';
+import LoginPasswordHoverCard from './LoginPasswordHoverCard';
+import LoginPasswordInput from './LoginPasswordInput';
 
 const LoginForm = () => {
-  const { register } = useForm();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(LoginSchema), defaultValues: { email: '', password: '' } });
+
+  const submitAction = (data: Login) => {
+    console.log('submit');
+    console.log(data);
+  };
 
   return (
     <div className="w-1/2 flex flex-col text-center items-center justify-center">
@@ -13,10 +27,24 @@ const LoginForm = () => {
         <h2 className="text-3xl">Log in</h2>
         <p>Join modern trading platform now.</p>
       </div>
-      <form className="w-full flex flex-col items-center justify-center gap-8">
-        <Input className="max-w-[250px]" placeholder="example@example.com" type="email" />
-        <Input className="max-w-[250px]" placeholder="password" type="password" />
-
+      <form
+        className="w-full flex flex-col items-center justify-center gap-2"
+        onSubmit={handleSubmit(submitAction)}
+      >
+        <Input
+          className="max-w-[280px]"
+          placeholder="example@example.com"
+          type="email"
+          {...register('email')}
+        />
+        {errors.email ? (
+          <p className="text-xs text-red-500">{errors.email.message}</p>
+        ) : (
+          <div className="h-4"></div>
+        )}
+        <div className="flex flex-col w-[280px] items-stretch">
+          <LoginPasswordInput control={control} />
+        </div>
         <Button variant="secondary" className="cursor-pointer" type="submit">
           Log in
         </Button>
---
+++ src/components/app/login/LoginPasswordHoverCard.tsx
@@ -0,0 +1,33 @@
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
+import { cn } from '@/lib/utils';
+import { BsFillInfoSquareFill } from 'react-icons/bs';
+
+type LoginPasswordHoverCard = {
+  value: string;
+};
+
+const LoginPasswordHoverCard = ({ value }: LoginPasswordHoverCard) => {
+  const fiveCharactes = /^.{5,}$/.test(value);
+  const oneSpecialCharacter = /^(?=.*[!@#$%^&*])/.test(value);
+  const oneUppercaseCharacter = /[A-Z]/.test(value);
+
+  return (
+    <HoverCard>
+      <HoverCardTrigger className="cursor-pointer">
+        <BsFillInfoSquareFill />
+      </HoverCardTrigger>
+      <HoverCardContent>
+        <p className={cn('text-xs', fiveCharactes ? 'text-green-500' : 'text-red-500')}>
+          At least 5 characters
+        </p>
+        <p className={cn('text-xs', oneUppercaseCharacter ? 'text-green-500' : 'text-red-500')}>
+          At least 1 uppercase character
+        </p>
+        <p className={cn('text-xs', oneSpecialCharacter ? 'text-green-500' : 'text-red-500')}>
+          At least 1 special character
+        </p>
+      </HoverCardContent>
+    </HoverCard>
+  );
+};
+export default LoginPasswordHoverCard;
